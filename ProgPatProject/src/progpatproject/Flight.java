e276add25cc1842511ea3e9c622450644d7083ab
Christian Kfoury <christian_kfoury558@hotmail.ca>
2021-05-12 10:02:49 -0400

    Fixed database connection to AirLines.db and made little changes on flights

--- ProgPatProject/src/progpatproject/Flight.java
+++ ProgPatProject/src/progpatproject/Flight.java
@@ -13,7 +13,7 @@ import java.util.TreeMap;
  */
 public class Flight {
 
-    private Connection connection = SingleConnection.getFlightsInstance();
+    private Connection connection = SingleConnection.getInstance();
     private String flightN;
     private String name;
     private String origin;
@@ -37,6 +37,7 @@ public class Flight {
 
     public boolean addFlight(Flight flight) {
         try {
+            // Checking if flight already exists
             Statement statement = connection.createStatement();
             String queryTable = String.format("SELECT COUNT(*) FROM Flights WHERE "
                     + "FlightN = '%s;", flight.getFlightN());
@@ -53,6 +54,7 @@ public class Flight {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
+        // if flight does not already exist in the database insert a new row
         try {
             Statement statement = connection.createStatement();
             String insertInTable = String.format("INSERT INTO FLIGHTS (FlightN, Name, Origin,"
@@ -61,16 +63,16 @@ public class Flight {
                     flight.getName(), flight.getOrigin(), flight.getDestination(),
                     flight.getDuration(), flight.getSeats(), flight.getAvailableSeats(), flight.getAmount());
             statement.executeUpdate(insertInTable);
-            return true;
         } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        return false;
+        return true;
     }
 
     public boolean removeFlight(String flightNumber) {
         try {
+            // Checking if flight does not exist
             Statement statement = connection.createStatement();
             String queryTable = String.format("SELECT COUNT(*) FROM Flights WHERE "
                     + "FlightN = '%s;", flightNumber);
@@ -88,27 +90,29 @@ public class Flight {
             System.exit(0);
         }
 
+        // if if flight exists, remove it
         try {
             Statement statement = connection.createStatement();
             String deleteInTable = String.format("DELETE FROM Flights WHERE FligtsN = %s", flightNumber);
             statement.executeUpdate(deleteInTable);
-            return true;
         } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        return false;
+        return true;
     }
 
     public boolean updateFlightData(String flightNumber, String field, String newValue) {
-        ArrayList<String> fields = new ArrayList<>(Arrays.asList("flightn", "name", "origin", "dest",
-                "duration", "seats", "available", "amount"));
-
-        if (!fields.contains(field.toLowerCase())) {
-            return false;
-        }
+        // SHOULD PUT THIS IN THE ACTUAL APPLICATION
+//        ArrayList<String> fields = new ArrayList<>(Arrays.asList("flightn", "name", "origin", "dest",
+//                "duration", "seats", "available", "amount"));
+//
+//        if (!fields.contains(field.toLowerCase())) {
+//            return false;
+//        }
 
         try {
+            // check if flight does not exist
             Statement statement = connection.createStatement();
             String queryTable = String.format("SELECT COUNT(*) FROM Flights WHERE "
                     + "FlightN = '%s;", flightNumber);
@@ -128,21 +132,33 @@ public class Flight {
 
         try {
             Statement statement = connection.createStatement();
-            String updateInTable = String.format("UPDATE Flights"
-                    + "SET '%s' = '%s'"
-                    + "WHERE '%s' = '%s'", field, newValue, field, flightNumber);
+            String updateInTable;
+
+            // depdending if the value is string or int/double, there should be
+            // quotations surronding the value
+            if (new ArrayList<>(Arrays.asList("flightsn", "name", "origin", "dest")).contains(field.toLowerCase())) {
+                updateInTable = String.format("UPDATE Flights"
+                        + "SET '%s' = '%s'"
+                        + "WHERE '%s' = '%s'", field, newValue, field, flightNumber);
+            } else {
+                updateInTable = String.format("UPDATE Flights"
+                        + "SET '%s' = %s"
+                        + "WHERE '%s' = '%s'", field, newValue, field, flightNumber);
+            }
+
             statement.executeUpdate(updateInTable);
-            return true;
         } catch (SQLException e) {
             System.err.println(e.getClass().getName() + ": " + e.getMessage());
             System.exit(0);
         }
-        return false;
+        return true;
     }
 
     public boolean issueTicket(String flight, Client client) {
         try {
             Statement statement = connection.createStatement();
+            // checking if there is available and selecting amount if there is available
+            // in one shot
             String queryTable = String.format("SELECT Available, Amount FROM Flights WHERE "
                     + "FlightN = '%s;", flight);
             ResultSet resultSet = statement.executeQuery(queryTable);
@@ -180,14 +196,16 @@ public class Flight {
     public boolean cancelFlight(int ticket, int passNumber) {
         try {
             Statement statement = connection.createStatement();
+            // check if ticket already exists
             String queryTable = String.format("SELECT COUNT(*) FROM ReservedFlights WHERE "
-                    + "TicketN = %d AND PassNum = %d;", ticket, passNumber);
+                    + "TicketN = %d;", ticket);
             ResultSet resultSet = statement.executeQuery(queryTable);
 
             int count = -1;
             while (resultSet.next()) {
                 count = resultSet.getInt(1);
             }
+            // if the ticket does not exist
             if (count == 0) {
                 return false;
             }
@@ -210,7 +228,7 @@ public class Flight {
 
     public static Map<String, String> viewBoard() {
         TreeMap<String, String> map = new TreeMap<>();
-        Connection connection = SingleConnection.getFlightsInstance();
+        Connection connection = SingleConnection.getInstance();
         try {
             Statement statement = connection.createStatement();
             String queryTable = String.format("SELECT * FROM Flights ORDER BY FlightN;");
@@ -229,9 +247,9 @@ public class Flight {
         return map;
     }
 
-    public static Map<String, String> viewBookedFlights(){
+    public static Map<String, String> viewBookedFlights() {
         TreeMap<String, String> map = new TreeMap<>();
-        Connection connection = SingleConnection.getFlightsInstance();
+        Connection connection = SingleConnection.getInstance();
         try {
             Statement statement = connection.createStatement();
             String queryTable = String.format("SELECT * FROM ReservedFlights ORDER BY FlightN;");
@@ -239,7 +257,7 @@ public class Flight {
 
             while (resultSet.next()) {
                 map.put("FlightN" + resultSet.getString("FlightN"), " TicketN" + resultSet.getString("TicketN")
-                        + ", PassNum" + resultSet.getString("PassNum") + ", FLName" + resultSet.getString("FLNAme")
+                        + ", PassNum" + resultSet.getString("PassNum") + ", FLName" + resultSet.getString("FLName")
                         + ", IssueDate" + resultSet.getString("IssueDate") + ", Contact" + resultSet.getString("Contact")
                         + ", Amount" + resultSet.getString("Amount"));
             }
@@ -249,7 +267,7 @@ public class Flight {
         }
         return map;
     }
-    
+
     public Connection getConnection() {
         return connection;
     }
